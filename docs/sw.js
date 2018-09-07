2a1385b8ec1507a16f1e894a8993c3cf23830f35
webkong <772930508@qq.com>
2018-09-07 15:29:00 +0800

    fix update pwa

--- docs/sw.js
+++ docs/sw.js
@@ -1,46 +1,63 @@
-var cacheName = 'v2';
+var cacheName = 'v3';
 
 self.addEventListener('install', function (event) {
-    event.waitUntil(
-        caches.open(cacheName).then(function (cache) {
-            cache.addAll([
-                '_assets',
-                '_media',
-                'zh-cn',
-                'README.md',
-                '_navbar.md'
-            ]);
-        }));
+  event.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      cache.addAll([
+        '_assets',
+        '_media',
+        'zh-cn',
+        'README.md',
+        '_navbar.md'
+      ]);
+    }));
 
 });
-self.addEventListener('activate', function(event){
-    console.log(event);
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    Promise.all([
+      // 更新客户端
+      self.clients.claim(),
+      // 清理旧版本
+      caches.keys().then(function (cacheList) {
+        return Promise.all(
+          cacheList.map(function (cacheName) {
+            if (cacheName !== cacheName) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    ])
+  );
 });
 
 self.addEventListener('fetch', function (event) {
-    event.respondWith(
-      caches.match(event.request, { ignoreSearch: true })
-        .then(function (response) {
-          if (response) {
+  event.respondWith(
+    caches.match(event.request, {
+      ignoreSearch: true
+    })
+    .then(function (response) {
+      if (response) {
+        return response;
+      }
+      var fetchRequest = event.request.clone();
+
+      return fetch(fetchRequest).then(
+        function (response) {
+          if (!response || response.status !== 200) {
             return response;
           }
-          var fetchRequest = event.request.clone();
-  
-          return fetch(fetchRequest).then(
-            function (response) {
-              if (!response || response.status !== 200) {
-                return response;
-              }
-  
-              var responseToCache = response.clone();
-              caches.open(cacheName)
-                .then(function (cache) {
-                  cache.put(event.request, responseToCache);
-                });
-  
-              return response;
-            }
-          );
-        })
-    );
-  });
+
+          var responseToCache = response.clone();
+          caches.open(cacheName)
+            .then(function (cache) {
+              cache.put(event.request, responseToCache);
+            });
+
+          return response;
+        }
+      );
+    })
+  );
+});
